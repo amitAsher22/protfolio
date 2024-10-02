db36127fae6a427c0da20f4816a25c8987c13c3c
amita <amitasher4@gmail.com>
2024-10-02 18:18:11 +0300

    dds

--- src/App.js
+++ src/App.js
@@ -6,81 +6,79 @@ import Header from "./components/Header";
 export const Context = React.createContext();
 
 function App() {
-  const [show, setshow] = useState(true);
+  const [show, setshow] = useState(false);
   const [color, setColor] = useState(true);
 
   return (
-    <Context.Provider>
-      <div
-        className={show !== true ? "darkApp" : "AppLight"}
-        id={color ? "fullScreenShow" : "fullScreenHide"}
-      >
-        <div className="header">
-          <Header />
-        </div>
-        <div className="body">
-          <div
-            id={color ? "fullScreenHide" : "fullScreenShow"}
-            className={show !== false ? "AppLight" : "darkApp"}
-          >
-            full screen
-          </div>
-
-          <div className="bodyhs">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
-          <div className="bodyhs">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
-          <div className="bodyhs">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
-          <div className="bodyhs">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
-          <div className="bodyhs">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
-          <div className="bodyhs">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
-
-          <div className="bodyhs">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
-          <div className="bodyhs">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
-          <div className="bodyhs">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
-          <div className="bodyhs">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
-        </div>
-        <div className="footer">footer</div>
+    <div
+      className={!show ? "darkApp" : "AppLight"}
+      id={!color ? "fullScreenShow" : "fullScreenHide"}
+    >
+      <div className="header">
+        <Header />
       </div>
-    </Context.Provider>
+      <div className="body">
+        <div
+          id={!color ? "fullScreenHide" : "fullScreenShow"}
+          className={show ? "AppLight" : "darkApp"}
+        >
+          full screen
+        </div>
+
+        <div className="bodyhs">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+        <div className="bodyhs">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+        <div className="bodyhs">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+        <div className="bodyhs">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+        <div className="bodyhs">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+        <div className="bodyhs">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+
+        <div className="bodyhs">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+        <div className="bodyhs">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+        <div className="bodyhs">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+        <div className="bodyhs">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+      </div>
+      <div className="footer">footer</div>
+    </div>
   );
 }
 
